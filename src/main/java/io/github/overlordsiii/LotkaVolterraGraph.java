a57c689562ff493733f87e6d68be8d564ae325a4
OverlordsIII <OverlordsIII@users.noreply.github.com>
2023-11-13 19:43:10 -0800

    add changes

--- src/main/java/io/github/overlordsiii/LotkaVolterraGraph.java
+++ src/main/java/io/github/overlordsiii/LotkaVolterraGraph.java
@@ -9,22 +9,44 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import javax.swing.*;
 
 public class LotkaVolterraGraph {
-    private DefaultCategoryDataset createDataset(double alpha, double beta, double gamma, double delta, double initialPrey, double initialPredator, int timeSteps) {
+    private DefaultCategoryDataset createDataset(double alpha, double beta, double gamma, double delta, double initialPrey, double initialPredator, int years) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
+        // this variable will track the amount of prey year-to-year
         double prey = initialPrey;
+
+        // this variable will track the amount of predators year-to-year
         double predator = initialPredator;
 
-        for (int t = 1; t <= timeSteps; t++) {
+        // we then plot the current prey value for initial population
+        dataset.addValue(prey, "Prey", "0");
+
+        // we then plot the current predator value for initial population
+        dataset.addValue(predator, "Predator", "0");
+
+        // this essentially runs per year, from 1 to 100 years in our case (specified by the years variable)
+        for (int t = 1; t <= years; t++) {
+
+            // this finds the change in prey per year. This is the equivalent to the lotka-volterra equation for prey
             double dPrey = alpha * prey - beta * prey * predator;
+
+            // this finds the change in predators per year. This is the equivalent to the lotka-volterra equation for predators
             double dPredator = delta * prey * predator - gamma * predator;
 
+            // we then add the change in prey to the prey after we calculate the prey
             prey += dPrey;
+
+            // we do the same for the predators
             predator += dPredator;
 
-            dataset.addValue(prey, "Prey", "Year " + t);
-            dataset.addValue(predator, "Predator", "Year " + t);
+            // we then plot the current prey value for the year we calculated
+            dataset.addValue(prey, "Prey", String.valueOf(t));
+
+            // we then plot the current predator value for the year we calculated
+            dataset.addValue(predator, "Predator", String.valueOf(t));
         }
+
+
 
         return dataset;
     }
@@ -33,15 +55,24 @@ public class LotkaVolterraGraph {
         DefaultCategoryDataset dataset = createDataset(alpha, beta, gamma, delta, initialPrey, initialPredator, timeSteps);
 
         JFreeChart chart = ChartFactory.createLineChart(
-                "Lotka-Volterra Model",
+                "Orca vs Sea Lion Population Predictions",
                 "Year",
-                "Population",
+                "Population (thousands)",
                 dataset,
                 PlotOrientation.VERTICAL,
                 true,
                 true,
                 false
         );
+
+        for (int r = 0; r < dataset.getRowCount(); r++) {
+            String series = (String) dataset.getRowKey(r);
+            for (int c = 0; c < dataset.getColumnCount(); c++) {
+                System.out.println(series
+                    + ", " + dataset.getColumnKey(c)
+                    + ", " + dataset.getValue(r, c));
+            }
+        }
 
         return chart;
     }
--- src/main/java/io/github/overlordsiii/Main.java
+++ src/main/java/io/github/overlordsiii/Main.java
@@ -7,46 +7,17 @@ import java.util.HashMap;
 public class Main {
 	public static void main(String[] args) {
 
-
-		/*
-        double alpha = 0.1;
-        double beta = 0.02;
-        double gamma = 0.3;
-        double delta = 0.01;
-        double initialPrey = 40;
-        double initialPredator = 9;
-        int timeSteps = 100;
-		 */
-
-		double alpha = 0.006218;
-		double beta = 0.03764;
-		double gamma = 0.1589;
-		double delta = 0.000006117;
-		double initialPrey = 45000;
-		double initialPredator = 21100;
-		int timeSteps = 10;
-
-		double prey = initialPrey;
-		double predator = initialPredator;
-
-		HashMap<Double, Integer> preyMap = new HashMap<>();
-		HashMap<Double, Integer> predatorMap = new HashMap<>();
-
-		for (int t = 1; t <= timeSteps; t++) {
-			double dPrey = alpha * prey - beta * prey * predator;
-			double dPredator = delta * prey * predator - gamma * predator;
-
-			prey += dPrey;
-			predator += dPredator;
-
-			preyMap.put(prey, t);
-			predatorMap.put(predator, t);
-		}
+		double alpha = 0.03674;
+		double beta = 0.01165;
+		double gamma = 0.1625;
+		double delta = 0.006053;
+		double initialPrey = 45;
+		double initialPredator = 21;
+		int timeSteps = 50;
 
 
-
-		//LotkaVolterraGraph lvGraph = new LotkaVolterraGraph();
-		//JFreeChart chart = lvGraph.createChart(alpha, beta, gamma, delta, initialPrey, initialPredator, timeSteps);
-		//lvGraph.displayChart(chart);
+		LotkaVolterraGraph lvGraph = new LotkaVolterraGraph();
+		JFreeChart chart = lvGraph.createChart(alpha, beta, gamma, delta, initialPrey, initialPredator, timeSteps);
+		lvGraph.displayChart(chart);
 	}
 }
